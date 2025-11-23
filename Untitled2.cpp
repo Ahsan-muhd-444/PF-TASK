f59d47a6bf7612817013680fe283abf17474ce38
MUHAMMAD AHSAN <ahsanmuhd444@gmail.com>
2025-11-24 01:27:05 +0500

    Add files via upload

--- Untitled2.cpp
+++ Untitled2.cpp
@@ -1,29 +1,26 @@
 #include <iostream>
 using namespace std;
-int main()
+int main ()
 {
-    int array;
- 
-     cout<<"Please enter array size = ";
-     cin>>array;
-     
-      if(array<=0 || array>10)
-      {
-      	cout<<"YOUR ARRAY SIZE REACHES MAX LIMIT (10)"<<endl;
-	  }
-     
-  int a[10];
-     for(int i=0 ; i< array; i++)
-     {
-     	
-     	cout<<"Enter value for[ " <<i<< " ] index : ";
-     	cin>>a[i];
-     	
-	 }
-	for(int i=0; i<array; i++)
-     	{
-     		cout<<"Value at [ "<< i <<" ] is = "<< a[i]<<endl;
-		 }
-
-
+	int array[4] = {10 , 20 , 30 , 40 };
+	
+	int i=0;
+	
+	
+	do{
+		{
+		cout<<array[i]<<endl;
+		i++;
+	}
+	
+	}
+		while(i<4);
+	
+	
+	
+	
+	
+	
+	
+	
 }
